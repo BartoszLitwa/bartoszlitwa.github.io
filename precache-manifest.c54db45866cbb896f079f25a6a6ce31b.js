27e2685d9cec4eeb67c251981ceaf274c770d2dc
BartoszLitwa <crytech.sim@gmail.com>
2022-10-11 22:03:28 +0200

    Updates

---
+++ precache-manifest.c54db45866cbb896f079f25a6a6ce31b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ef7f9a70f5c125840c1",
+    "url": "/static/css/main.6cc478f5.chunk.css"
+  },
+  {
+    "revision": "6ef7f9a70f5c125840c1",
+    "url": "/static/js/main.6ef7f9a7.chunk.js"
+  },
+  {
+    "revision": "bca6bc7a542ac4047426",
+    "url": "/static/css/1.27e743da.chunk.css"
+  },
+  {
+    "revision": "bca6bc7a542ac4047426",
+    "url": "/static/js/1.bca6bc7a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a41a4508ff29d96e67dde467e9190553",
+    "url": "/static/media/logo.a41a4508.png"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "b1478dc2026ea61d46011770298b0447",
+    "url": "/static/media/tasklists.b1478dc2.png"
+  },
+  {
+    "revision": "0a0f6dc8889f574722e154e532c88884",
+    "url": "/static/media/terrain.0a0f6dc8.png"
+  },
+  {
+    "revision": "af80b507dd176ef9c9881be6510f4b3c",
+    "url": "/static/media/introduction.af80b507.png"
+  },
+  {
+    "revision": "4f236663edb59d071ef65831e54cff5a",
+    "url": "/static/media/merged.4f236663.png"
+  },
+  {
+    "revision": "37d16b8967b4a3db71a82eb2cdb5bc22",
+    "url": "/static/media/merged.37d16b89.jpg"
+  },
+  {
+    "revision": "fa11b0b328289764b4ea8077bdab06e1",
+    "url": "/static/media/visualsTab.fa11b0b3.png"
+  },
+  {
+    "revision": "683d2afe87cabcfde6edeedccfb85901",
+    "url": "/static/media/loader.683d2afe.png"
+  },
+  {
+    "revision": "1313e159f64eb887eef8da9f659e66fc",
+    "url": "/static/media/snake in-game.1313e159.png"
+  },
+  {
+    "revision": "647284eb8f7454a4b370462033407a58",
+    "url": "/static/media/logo.647284eb.png"
+  },
+  {
+    "revision": "36560abc87210a13f5ad0d604388563f",
+    "url": "/static/media/colors.36560abc.jpg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "e8c0cca9f34d8b485b12bdfa6b4cc817",
+    "url": "/static/media/pzpl.e8c0cca9.png"
+  },
+  {
+    "revision": "8d0cbc598770cf4a02b604dc3b467bdf",
+    "url": "/index.html"
+  }
+];
